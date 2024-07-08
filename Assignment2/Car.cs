e59948770b6e71eba78ac0d423249bb11630bad4
abdelrahman-gaber1 <abdogaber487@gmail.com>
2024-07-09 00:41:02 +0300

    Demo

---
+++ Assignment2/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Car
+    {
+		private int id;
+		private string model;
+		private double speed;
+
+		public Car (int _id, string _model , double _speed)
+		{
+			id = _id;
+			model = _model;
+			speed = _speed;
+		}
+        public Car(int _id, string _model) :this(_id , _model, 180)
+        {
+            id = _id;
+            model = _model;
+            speed = 180;
+        }
+        public Car(int _id) : this(_id, "Marcedes")
+        {
+            id = _id;
+            model = "Marcedes";
+            speed = 180;
+        }
+
+        public double Speed
+		{
+			get { return speed; }
+			set { speed = value; }
+		}
+
+		public string Model
+		{
+			get { return model; }
+			set { model = value; }
+		}
+
+
+		public int ID
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+        public override string ToString()
+        {
+			return $"{id} ,  {speed}  ,   {model}";
+        }
+    }
+}
---
+++ Assignment2/Inhertinace/Parent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2.Inhertinace
+{
+    internal class Parent
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Parent(int _x , int _y)
+        {
+           X = _x;
+           Y = _y; 
+        }
+        public Parent()
+        {
+        }
+        public override string ToString()
+        {
+            return $"{X}, {Y}";
+        }
+
+        public long proudct()
+        {
+            return X * Y;
+        }
+    } 
+}
